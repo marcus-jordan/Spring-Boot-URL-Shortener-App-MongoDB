5a2c708aeda3d88041fe5cb0afec421556207b5f
Marcus <demathamarcus@gmail.com>
2020-03-21 19:41:09 -0400

    Added create url service call to test suite

--- src/test/java/com/shortener/url/example/ExampleApplicationTests.java
+++ src/test/java/com/shortener/url/example/ExampleApplicationTests.java
@@ -1,56 +1,57 @@
 package com.shortener.url.example;
 
 import org.junit.jupiter.api.*;
-
 import org.junit.jupiter.api.extension.ExtendWith;
-import com.shortener.url.example.model.UrlMapping;
 import com.shortener.url.example.repositories.UrlMappingRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.shortener.url.example.services.UrlService;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles(profiles = {"test"})
 class UrlShortenerTests {
-	@Autowired
+
 	private UrlMappingRepository urlMappingRepository;
-	
+	private UrlService urlService;
+
+	@Autowired
+	UrlShortenerTests(UrlMappingRepository urlMappingRepository, UrlService urlService) {
+		this.urlMappingRepository = urlMappingRepository;
+		this.urlService = urlService;
+	}
+
 	@BeforeAll
 	public void setup() throws Exception {
-		List<UrlMapping> urlMappings = new ArrayList<UrlMapping>();
-		urlMappings.add(new UrlMapping("jsu", "jacksonstateuniversity.edu"));
-		urlMappings.add(new UrlMapping("umd", "universityofmaryland.edu"));
-
-		urlMappingRepository.insert(urlMappings);
+		urlService.createUrl("jsu", "jacksonstateuniversity.edu");
+		urlService.createUrl("umd", "universityofmaryland.edu");
+		Assertions.assertAll(
+				() -> Assertions.assertTrue(urlMappingRepository.existsById("jsu")),
+				() -> Assertions.assertTrue(urlMappingRepository.existsById("umd"))
+		);
 	}
 	@Test
 	void testCreateMapping() {
-		createUrlMapping("gmu", "georgemasonuniversity.edu");
+		urlService.createUrl("gmu", "georgemasonuniversity.edu");
 		Assertions.assertTrue(urlMappingRepository.existsById("gmu"));
 	}
 	@Test
 	void testDuplicateKey() {
-		createUrlMapping("duplicate", "www.east.com");
-		Assertions.assertThrows(DuplicateKeyException.class, () -> createUrlMapping("duplicate", "west.com"));
+		urlService.createUrl("duplicate", "www.east.com");
+		Assertions.assertThrows(DuplicateKeyException.class, () -> urlService.createUrl("duplicate", "west.com"));
 	}
 	@Test
 	void testDeleteMapping() {
-		createUrlMapping("map", "mapping.com");
+		urlService.createUrl("map", "mapping.com");
 		urlMappingRepository.deleteById("map");
 		Assertions.assertFalse(urlMappingRepository.existsById("map"));
 	}
-	private void createUrlMapping(String key, String value){
-		urlMappingRepository.insert(new UrlMapping(key, value));
-	}
 	@AfterEach
-	void teardown(){
+	void teardown() {
 		urlMappingRepository.deleteAll();
 	}
 }
